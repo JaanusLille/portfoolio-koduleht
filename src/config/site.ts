06e470c3790c0671f276b74625b18702ce04b876
Jaanus Lille <jaanuslille@gmail.com>
2026-03-23 08:18:22 +0200

    Label correction

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 /** Edit these values to personalize your portfolio. */
 export const site = {
   name: 'Your Name',
-  role: 'Web developer',
+  role: 'Software developer',
   tagline: 'Interfaces that feel as good as they look.',
   intro:
     'I design and build fast, accessible web experiences. Here you can explore selected work and how I approach each project.',
